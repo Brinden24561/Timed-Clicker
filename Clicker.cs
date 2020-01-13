cc5f4a35e3c5246e97196210557beeb8133e3489
Brinden24561 <39349733+Brinden24561@users.noreply.github.com>
2020-01-13 12:59:23 -0600

    Add files via upload

---
+++ Clicker.cs
@@ -0,0 +1,17 @@
+﻿using AutoClicker.Model;
+using AutoClicker.Model.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Threading;
+
+namespace AutoClicker.Model
+{
+    public class Clicker : Observable
+    {
+        /// <summary>
+        /// Should probably refactor this.
+        /// </summary>
+    }
+}
