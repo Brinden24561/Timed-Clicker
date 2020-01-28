b360447a18bd7f0f809bffe9adabfb84b33aea1d
Brinden24561 <39349733+Brinden24561@users.noreply.github.com>
2020-01-27 21:22:27 -0600

    C#
    Version add-on includes ClickCounter

---
+++ AutoClicker/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System;
+
+namespace SortingApplication
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+          //  this.Suspending  += OnSuspending;
+        }
+        //private void OnSuspending(object sender, SuspendingEventArgs e)
+        //{
+
+        //}
+    }
+}
---
+++ AutoClicker/ViewModels/MainClickerView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Windows.Input;
+using System.Diagnostics;
+using AutoClicker.Model;
+namespace AutoClicker.ViewModel
+{
+    public class MainClickerView : Model.Base.Observable
+    {
+        
+    }
+}
